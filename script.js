51c4ad625798f507b1cdcb2e235ae334f0b09f09
catmoonsn <minemail775@gmail.com>
2026-05-16 20:27:42 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ image.addEventListener("click", () => {
         // 表示
         textArea.classList.remove("hidden");
 
-    }, 10);
+    }, 200);
 
     // 一回制限保存
     sessionStorage.setItem("drawn", "yes");
